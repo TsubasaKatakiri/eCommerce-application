7856857dd02cff1afa95a003dc2f26d46d236583
Tsubasa <katakiritsubasa@gmail.com>
2025-05-19 23:29:03 +0300

    feat: change some settings

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './app.tsx';
 import { Provider } from 'react-redux';
@@ -9,9 +9,9 @@ import { store } from './store/configure-store.ts';
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );
