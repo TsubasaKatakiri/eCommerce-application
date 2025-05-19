0b451bc6db266b455f03034d4d7d0d8147534240
Rixels <mikey.drozd@gmail.com>
2025-05-19 18:13:50 +0300

    fix: innacuracies in the merger have been removed

--- ecommerce-application/src/App.tsx
+++ ecommerce-application/src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Header from './header';
-<<<<<<< HEAD
-
-import LoginPage from './loginPage'
-import SignUpPage from './signUpPage'
-
-import HomePage from './homePage';
-
+import RegistrationPage from './register'; 
+import LoginPage from './loginPage';
+import HomePage from './homePage'; 
 
 function App() {
   return (
@@ -16,42 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<SignUpPage />} />
-      </Routes>
-    </>
-  );
-}
-
-=======
-import RegistrationPage from './register'
-import LoginPage from './loginPage'
-
-
-function Home() {
-  return (
-    <div>
-      <h1>Vite + React</h1>
-      <h2>главная страница</h2>
-      <div className="card">
-      </div>
-    </div>
-  );
-}
-
-
-
-function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
       </Routes>
     </>
   );
 }
 
->>>>>>> rss-ecomm-2-16
-export default App
+export default App;
